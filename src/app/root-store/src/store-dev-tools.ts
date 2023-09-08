a07b8813ed0fb440787cadc6011a71f86aeab0f3
Cristian Rossi <c.rossi@teamsystem.com>
2023-09-08 15:56:14 +0200

    feat(store): redux devtools works

---
+++ src/app/root-store/src/store-dev-tools.ts
@@ -0,0 +1,5 @@
+import { StoreDevtoolsOptions } from '@ngrx/store-devtools';
+export const storeDevToolsOptionsForDev: StoreDevtoolsOptions = {
+  name: 'DevCalendar NgRx Store DevTools',
+  maxAge: 25,
+};
