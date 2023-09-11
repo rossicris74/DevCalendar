a118a06555d3594677481ccfd4a1bd89970b7e72
Cristian Rossi <c.rossi@teamsystem.com>
2023-09-11 10:58:38 +0200

    feat(icons): sistemato fontwasome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit, enableProdMode } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import * as TestActions from './root-store/src/test/test.actions';
 import * as TestState from './root-store/src/test/state';
-import * as fromRouter from '@ngrx/router-store';
-import { RuntimeChecks } from '@ngrx/store';
+import * as TestActions from './root-store/src/test/test.actions';
 import * as TestSelectors from './root-store/src/test/test.selectors';
 
 @Component({
@@ -11,21 +9,19 @@ import * as TestSelectors from './root-store/src/test/test.selectors';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
 })
-export class AppComponent implements OnInit{
-getId$ = this.testStore.select(TestSelectors.getId);
-getDescr$ = this.testStore.select(TestSelectors.getDescr);
-  constructor(
-    private readonly testStore: Store<TestState.State>
-) {}
+export class AppComponent implements OnInit {
+  getId$ = this.testStore.select(TestSelectors.getId);
+  getDescr$ = this.testStore.select(TestSelectors.getDescr);
+  constructor(private readonly testStore: Store<TestState.State>) {}
 
-ngOnInit(){
-this.testStore.dispatch(
-  TestActions.setTest({
-      id:0,
-      descr:'Test'
-  })
-);
-this.getId$.subscribe((id) => console.log('id: '+ id));
-this.getDescr$.subscribe((descr) => console.log('descr: ' + descr));
-}
+  ngOnInit() {
+    this.testStore.dispatch(
+      TestActions.setTest({
+        id: 0,
+        descr: 'Test',
+      })
+    );
+    this.getId$.subscribe((id) => console.log('id: ' + id));
+    this.getDescr$.subscribe((descr) => console.log('descr: ' + descr));
+  }
 }
