67bb005a88e3dfa228a7bdb3db13143885715a41
rossicris74 <rossicris74@gmail.com>
2023-12-04 21:03:07 +0100

    feat(appointments): get and model works

--- src/app/components/dev-calendar/dev-calendar.module.ts
+++ src/app/components/dev-calendar/dev-calendar.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DxSchedulerModule, DxSwitchModule } from 'devextreme-angular';
+import { DxSchedulerModule, DxSwitchModule, DxButtonModule } from 'devextreme-angular';
 import { DevCalendarComponent } from './dev-calendar.component';
 
 @NgModule({
   declarations: [DevCalendarComponent],
-  imports: [DxSchedulerModule, DxSwitchModule, CommonModule],
+  imports: [DxSchedulerModule, DxSwitchModule, DxButtonModule, CommonModule],
   exports:[DevCalendarComponent],
   providers: [],
 })
